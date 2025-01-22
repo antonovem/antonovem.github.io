2031d700f902c97901b1f7d55705975ba6f6262f
antonovem <yaantonov76@yandex.ru>
2025-01-22 21:36:12 +0300

    homework1

--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -5,52 +5,112 @@
  * В целом сделайте так, как вам будет удобно.
  * */
 
-/**
- * Нужно создать тип Category, он будет использоваться ниже.
- * Категория содержит
- * - id (строка)
- * - name (строка)
- * - photo (строка, необязательно)
- *
- * Продукт (Product) содержит
- * - id (строка)
- * - name (строка)
- * - photo (строка)
- * - desc (строка, необязательно)
- * - createdAt (строка)
- * - oldPrice (число, необязательно)
- * - price (число)
- * - category (Категория)
- *
- * Операция (Operation) может быть либо тратой (Cost), либо доходом (Profit)
- *
- * Трата (Cost) содержит
- * - id (строка)
- * - name (строка)
- * - desc (строка, необязательно)
- * - createdAt (строка)
- * - amount (число)
- * - category (Категория)
- * - type ('Cost')
- *
- * Доход (Profit) содержит
- * - id (строка)
- * - name (строка)
- * - desc (строка, необязательно)
- * - createdAt (строка)
- * - amount (число)
- * - category (Категория)
- * - type ('Profit')
- * */
+type TCategory = {
+  id: string;
+  name: string;
+  photo?: string;
+};
+
+type TProduct = {
+  id: string;
+  name: string;
+  photo: string;
+  desc?: string;
+  createdAt: string;
+  oldPrice?: number;
+  price: number;
+  category: TCategory;
+};
+
+type TCost = {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: TCategory;
+  type: 'Cost';
+};
+
+type TProfit = {
+  id: string;
+  name: string;
+  desc?: string;
+  createdAt: string;
+  amount: number;
+  category: TCategory;
+  type: 'Profit';
+};
 
 /**
  * Создает случайный продукт (Product).
  * Принимает дату создания (строка)
  * */
-// export const createRandomProduct = (createdAt: string) => {};
+
+const getRandomNumberInRange = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const getRandomName = (): string => {
+  // eslint-disable-next-line prettier/prettier
+  const names = [
+    'Audi',
+    'Wolkswagen',
+    'ВАЗ',
+    'ГАЗ',
+    'ТАЗ',
+    'Кофемолка',
+    'Эспандер'
+  ];
+
+  return names[getRandomNumberInRange(0, names.length - 1)];
+};
+
+export const createRandomProduct = (createdAt: string): TProduct => {
+  const product: TProduct = {
+    id: getRandomNumberInRange(1, 100).toString(),
+    name: getRandomName(),
+    photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQpzn2GyJaYRjG_CoiZdQM2cmlKvMvk00vJQ&s',
+    createdAt: createdAt,
+    price: getRandomNumberInRange(5001, 10000),
+    category: {
+      id: '78',
+      name: 'Other',
+    },
+  };
+
+  if (getRandomNumberInRange(0, 10) > 5) {
+    product.desc = 'Какой замечательный продукт ' + product.name + ' всего за символические ' + product.price + ' у.е.';
+  }
+
+  if (getRandomNumberInRange(0, 10) > 5) {
+    product.oldPrice = getRandomNumberInRange(0, 5000);
+  }
+
+  return product;
+};
+// По аналогии с getRandomName делаем для photo, category.Вариации разные. Отдельно массивы, а функция одна т.к. выборка из массива или отдельные функции для каждого поля.
+
+type TOperation = TCost | TProfit;
 
 /**
  * Создает случайную операцию (Operation).
  * Принимает дату создания (строка)
  * */
-// export const createRandomOperation = (createdAt: string) => {};
+export const createRandomOperation = (createdAt: string): TOperation => {
+  const id = getRandomNumberInRange(0, 100);
+  const isCost = getRandomNumberInRange(0, 10) > 5;
+
+  return {
+    id: id.toString(),
+    name: 'Операция номер: ' + id,
+    desc: isCost ? 'Потрачено на' : 'Доход с',
+    createdAt: createdAt,
+    amount: getRandomNumberInRange(1, 10000),
+    category: {
+      id: isCost ? '78' : '80',
+      name: isCost ? 'Потрачено' : 'Доход',
+    },
+    type: isCost ? 'Cost' : 'Profit',
+  };
+};
